b52913a598b061ced6793f4709329ceb4466587e
islemamirhassen <islem.ahmed92@gmail.com>
2024-05-09 22:20:52 +0200

    adding model

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,7 +1,6 @@
 import { ArticleService } from './article.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ArticleRoutingModule } from './article-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
